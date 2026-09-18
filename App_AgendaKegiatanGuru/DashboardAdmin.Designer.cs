f9d475ac1230bf10c5ca5d8eb9d89fb31c626884
FaizFadhilah253 <faizfadhillah20@gmail.com>
2026-09-18 15:03:46 +0700

    perubahan form laporan

--- App_AgendaKegiatanGuru/DashboardAdmin.Designer.cs
+++ App_AgendaKegiatanGuru/DashboardAdmin.Designer.cs
@@ -201,7 +201,7 @@ namespace App_AgendaKegiatanGuru
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.label5.Location = new System.Drawing.Point(133, 64);
+            this.label5.Location = new System.Drawing.Point(132, 64);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(32, 32);
             this.label5.TabIndex = 4;
--- App_AgendaKegiatanGuru/DashboardAdmin.cs
+++ App_AgendaKegiatanGuru/DashboardAdmin.cs
@@ -88,7 +88,7 @@ namespace App_AgendaKegiatanGuru
 
         private void label8_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void label6_Click(object sender, EventArgs e)
